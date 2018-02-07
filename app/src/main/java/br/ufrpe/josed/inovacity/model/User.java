f0f2f96dc6287546123e5d18c99e4cfd2622ce13
José Antônio <josedossantos@users.noreply.github.com>
2018-02-07 20:26:33 -0300

    Classe user criada

---
+++ app/src/main/java/br/ufrpe/josed/inovacity/model/User.java
@@ -0,0 +1,16 @@
+package br.ufrpe.josed.inovacity.model;
+
+
+/**
+ * Created by josed on 07/02/2018.
+ */
+
+public class User {
+
+    private long id;
+    private String nome;
+    private String senha;
+
+public void user(){}
+
+}
